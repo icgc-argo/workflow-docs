990a16252226b3e64cbff0a183fb7821aeead794
Alexis Li <alexis.li@oicr.on.ca>
2020-01-10 15:48:48 -0500

    make HOST configurable

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || 'wes.light.overture.bio';
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./merged.json');
+
+swaggerDocument.host= HOST;
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
